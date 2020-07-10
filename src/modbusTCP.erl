4964f834230ab27930980b8bcf6298fff58d108c
Semyon Kekeev <52729573+cemen77763@users.noreply.github.com>
2020-07-10 15:26:39 +0700

    version == 1.2.7
    Added reaad/write holding registers, read input registers, read coils.

--- src/modbusTCP.erl
+++ src/modbusTCP.erl
@@ -5,8 +5,8 @@
     {ok, State :: term(), NetInfo :: list()} | {ok, State :: term(), NetInfo :: term(), timeout()} |
     {stop, Reason :: term()} | ignore.
 
--callback connect(State :: term()) ->
-    {ok, NewState :: term(), NewNetInfo :: list()} | {stop, Reason :: term(), NewState :: term()} | {error, Reason :: term()}.
+-callback connect(State :: term(), Info :: ({connected, Ip_addr :: term(), Port :: integer()} | {error, Reason :: term()})) ->
+    {ok, NewState :: term(), Info :: term()} | {stop, Reason :: term(), NewState :: term()} | {error, Reason :: term()}.
 
 -callback disconnect(State :: term()) ->
     {ok, NewState :: term()} | {stop, NewState :: term()}.
@@ -14,10 +14,13 @@
 -callback message(RegisterInfo :: list(), State :: term()) ->
     {reply, Reply :: term(), NewState :: term()} | {noreply, NewState :: term()} | {stop, NewState :: term()}.
 
+-callback terminate_modbus(Reason :: (normal | shutdown | {shutdown, term()} | term()), State :: term()) -> 
+    term().
+
+-optionalcallbacks([terminate_modbus/2]).
+
 
 -behaviour(gen_server).
-
--include("modbus_functional_codes.hrl").
 
 % API
 -export([
@@ -38,6 +41,8 @@
     terminate/2, 
     code_change/3]).
 
+-include("modbus_functional_codes.hrl").
+
 -record(modbusTCP, {
         socket = 0,
         connection = close,
@@ -57,7 +62,7 @@
 
 
 start_link(Mod, Args, Options) ->
-    gen_server:start_link(Mod, [Mod, Args], Options).
+    gen_server:start_link(?MODULE, [Mod, Args], Options).
 
 
 start_link(Name, Mod, Args, Options) ->
@@ -67,30 +72,44 @@ start_link(Name, Mod, Args, Options) ->
 stop(Name) ->
     gen_server:stop(Name).
 
+
 try_connect(Name, NetInfo) ->
     gen_server:cast(Name, {try_connect, NetInfo}).      
+
 
 try_disconnect(Name) ->
     gen_server:cast(Name, {try_disconnect}).
 
+
 read_register(Name, RegisterInfo) ->
     gen_server:cast(Name, {read, RegisterInfo}).
+
 
 write_register(Name, RegisterInfo) ->
     gen_server:cast(Name, {write, RegisterInfo}).
 
 
 init([Mod, Args]) ->
-    {ok, {ok, State, [Ip_addr, Port]}} = 
-    try
-        {ok, Mod:init_modbus(Args)}
+    case init_it(Mod, Args) of
+        {ok, {ok, State, _NetInfo}} ->  
+            {ok, #state{mod = Mod, state = State}};
 
-    catch 
-        throw:R -> {ok, R};
-        Class:R:S -> {'EXIT', Class, R, S}
+        {ok, {ok, State, _NetInfo, Timeout}} ->    
+            {ok, #state{mod = Mod, state = State}, Timeout};
 
-    end, 
-    {ok, #state{modbus_state = #modbusTCP{ip_addr = Ip_addr, port = Port}, state = State, mod = Mod}}.
+        {ok, {stop, Reason}} ->
+            {stop, Reason};
+
+        {ok, ignore} ->
+            ignore;
+
+        {ok, Else} ->
+            Else;
+
+        {'EXIT', Class, Reason, Stacktrace} ->
+            erlang:raise(Class, Reason, Stacktrace)
+    
+    end.
 
 
 handle_call(stop, _From, State) ->
@@ -101,23 +120,42 @@ handle_call(_Request, _From, State) ->
 
 handle_cast({try_connect, [Ip_addr, Port]}, State) ->
     Mod = State#state.mod,
-    NewState = try_reconnect(State, [Ip_addr, Port]),
-    {ok, {ok, ModbusState, _NetInfo}} = 
-    try
-        {ok, Mod:connect(State#state.state)}
+    NewState = try_reconnect(State, [Ip_addr, Port], 0),
+    case NewState of
+        {error, Reason} ->
+            try
+            {ok, Mod:connect(State#state.state, {error, Reason})}
 
-    catch 
-        throw:R -> {ok, R};
-        Class:R:S -> {'EXIT', Class, R, S}
+            catch 
+                throw:R -> {ok, R};
+                Class:R:S -> {'EXIT', Class, R, S}
 
-    end, 
-    
-    {noreply, NewState#state{state = ModbusState}};
+            end,
+            {noreply, State};
+        
+        _Other -> 
+            {ok, {ok, ModbusState, _NetInfo}} = 
+            try
+                {ok, Mod:connect(State#state.state, {connected, Ip_addr, Port})}
+        
+            catch 
+                throw:R -> {ok, R};
+                Class:R:S -> {'EXIT', Class, R, S}
+        
+            end,
+            {noreply, NewState#state{state = ModbusState}}
+    end;
 
 handle_cast({try_disconnect}, State) ->
     Mod = State#state.mod,
-    gen_tcp:close(State#state.modbus_state#modbusTCP.socket),
+    Socket = State#state.modbus_state#modbusTCP.socket,
+    if Socket =/= 0 ->
+        gen_tcp:close(State#state.modbus_state#modbusTCP.socket);
+        true ->
+            ok
+    end,
 
+    {ok, {ok, NewState}} = 
     try
         {ok, Mod:disconnect(State#state.state)}
 
@@ -127,13 +165,14 @@ handle_cast({try_disconnect}, State) ->
 
     end, 
     
-    {noreply, State#state{modbus_state = #modbusTCP{socket = 0, connection = close}}};
+    {noreply, State#state{state = NewState, modbus_state = #modbusTCP{socket = 0, connection = close}}};
 
 handle_cast({read, {holding_register, Dev_num, Reg_num}}, State) ->
     Packet = <<1:16, 0:16, 6:16, Dev_num:8, ?FUN_CODE_READ_HREGS:8, Reg_num:16, 1:16>>,
     Socket = State#state.modbus_state#modbusTCP.socket,
+    Mod = State#state.mod,
+
     if Socket =/= 0 ->
-        Mod = State#state.mod,
 
         % Modbus код функции 03 (чтение Holding reg)
         case gen_tcp:send(Socket, Packet) of
@@ -142,36 +181,117 @@ handle_cast({read, {holding_register, Dev_num, Reg_num}}, State) ->
                 {noreply, State};
 
             {error, _Reason} -> 
-                Mod:message([error, "can't send message"], State#state.state),
+                Mod:message({error, cant_send}, State#state.state),
                 {noreply, State}
 
         end;
-    true -> {noreply, State}
+
+    true -> 
+        Mod:message({error, socket_closed}, State#state.state),
+        {noreply, State}
+
+    end;
+
+handle_cast({read, {holding_register, Dev_num, Reg_num, Quantity}}, State) ->
+    Packet = <<1:16, 0:16, 6:16, Dev_num:8, ?FUN_CODE_READ_HREGS:8, Reg_num:16, Quantity:16>>,
+    Socket = State#state.modbus_state#modbusTCP.socket,
+    Mod = State#state.mod,
+
+    if Socket =/= 0 ->
+
+        % Modbus код функции 03 (чтение Holding reg)
+        case gen_tcp:send(Socket, Packet) of
+            ok ->
+                reading_holding_registers(Socket, State, [Dev_num, Reg_num, Quantity]),
+                {noreply, State};
+
+            {error, _Reason} -> 
+                Mod:message({error, cant_send}, State#state.state),
+                {noreply, State}
+
+        end;
+
+    true -> 
+        Mod:message({error, socket_closed}, State#state.state),
+        {noreply, State}
+
+    end;
+
+handle_cast({read, {input_register, Dev_num, Reg_num, Quantity}}, State) ->
+    Packet = <<1:16, 0:16, 6:16, Dev_num:8, ?FUN_CODE_READ_IREGS:8, Reg_num:16, Quantity:16>>,
+    Socket = State#state.modbus_state#modbusTCP.socket,
+    Mod = State#state.mod,
+
+    if Socket =/= 0 ->
+
+        % Modbus код функции 04 (чтение нескольких Input regs)
+        case gen_tcp:send(Socket, Packet) of
+            ok ->
+                reading_input_registers(Socket, State, [Dev_num, Reg_num, Quantity]),
+                {noreply, State};
+
+            {error, _Reason} -> 
+                Mod:message({error, cant_send}, State#state.state),
+                {noreply, State}
+
+        end;
+
+    true -> 
+        Mod:message({error, socket_closed}, State#state.state),
+        {noreply, State}
+
+    end;
+
+handle_cast({read, {coil_status, Dev_num, Reg_num}}, State) ->
+    Packet = <<1:16, 0:16, 4:16, Dev_num:8, ?FUN_CODE_READ_COILS:8, Reg_num:16>>,
+    Socket = State#state.modbus_state#modbusTCP.socket,
+    Mod = State#state.mod,
+
+    if Socket =/= 0 ->
+
+        % Modbus код функции 01 (чтение нескольких Coil status)
+        case gen_tcp:send(Socket, Packet) of
+            ok ->
+                reading_coil_status(Socket, State, [Dev_num, Reg_num]),
+                {noreply, State};
+
+            {error, _Reason} -> 
+                Mod:message({error, cant_send}, State#state.state),
+                {noreply, State}
+
+        end;
+
+    true -> 
+        Mod:message({error, socket_closed}, State#state.state),
+        {noreply, State}
 
     end;
 
 handle_cast({write, {holding_register, Dev_num, Reg_num, Values}}, State) ->
     Socket = State#state.modbus_state#modbusTCP.socket,
+    Mod = State#state.mod,
+
     if Socket =/= 0 ->
         Reg_quantity = length(Values),
         Len = Reg_quantity * 2,
     
         Packet_without_values = <<1:16, 0:16, 16#0B:16, Dev_num:8, ?FUN_CODE_WRITE_HREGS:8, Reg_num:16, Reg_quantity:16, Len:8>>,
         PacketMsg = list_to_bin16(Values, Packet_without_values),
-        Mod = State#state.mod,
 
-        % Modbus код функции 03 (чтение Holding reg)
+        % Modbus код функции 10 (запись нескольких Holding regs)
         case gen_tcp:send(Socket, PacketMsg) of
             ok ->
                 writing_holding_register(Socket, State, [Dev_num, Reg_num, Values]),
                 {noreply, State};
 
             {error, _Reason} -> 
-                Mod:message([error, "can't send message"], State#state.state),
+                Mod:message({error, cant_send}, State#state.state),
                 {noreply, State}
 
         end;
-    true -> {noreply, State}
+    true -> 
+        Mod:message({error, socket_closed}, State#state.state),
+        {noreply, State}
 
     end;
 
@@ -181,24 +301,102 @@ handle_cast(_Msg, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
-terminate(_Reason, _State) ->
+terminate(Reason, State) ->
+    Mod = State#state.mod,
+    try_terminate(Mod, Reason, State#state.state),
+    Socket = State#state.modbus_state#modbusTCP.socket,
+    if Socket =/= 0 ->
+        gen_tcp:close(State#state.modbus_state#modbusTCP.socket);
+        true ->
+            ok
+    end,
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
-% ------------------------------------------------------------------------
+% -------------------------------------------------------------------------------
+
+init_it(Mod, Args) ->
+    try
+        {ok, Mod:init_modbus(Args)}
+
+    catch 
+        throw:R -> {ok, R};
+        Class:R:S -> {'EXIT', Class, R, S}
+
+    end.
+
+try_terminate(Mod, Reason, State) ->
+    case erlang:function_exported(Mod, terminate_modbus, 2) of
+	true ->
+	    try
+		    {ok, Mod:terminate_modbus(Reason, State)}
+	    
+        catch
+		    throw:R ->
+            {ok, R};
+            
+		Class:R:Stacktrace ->
+            {'EXIT', Class, R, Stacktrace}
+            
+        end;
+	false ->
+        {ok, ok}
+        
+    end.
 
 reading_holding_register(Socket, State, [Dev_num, Reg_num]) ->
     Mod = State#state.mod,
 
-    case gen_tcp:recv(Socket, 11, 3000) of
+    case gen_tcp:recv(Socket, 0, 3000) of
         {ok, Data} -> 
             <<1:16, 0:16, 5:16, Dev_num:8, ?FUN_CODE_READ_HREGS:8, 2:8, Reg_value:16>> = Data,
-            Mod:message([Dev_num, Reg_num, Reg_value], State#state.state);
+            Mod:message({holding_register, Dev_num, Reg_num, Reg_value}, State#state.state);
 
         {error, Reason} ->
-            Mod:message([error, Reason], State#state.state)
+            Mod:message({error, Reason}, State#state.state)
+
+    end.
+
+reading_holding_registers(Socket, State, [Dev_num, Reg_num, _Quantity]) ->
+    Mod = State#state.mod,
+
+    case gen_tcp:recv(Socket, 0, 3000) of
+        {ok, Data} -> 
+            <<1:16, 0:16, _:16, Dev_num:8, ?FUN_CODE_READ_HREGS:8, _:8, BinData/binary>> = Data,
+            LData = bin_to_list16(BinData, []),
+            Mod:message({holding_register, Dev_num, Reg_num, LData}, State#state.state);
+
+        {error, Reason} ->
+            Mod:message({error, Reason}, State#state.state)
+
+    end.
+
+reading_input_registers(Socket, State, [Dev_num, Reg_num, _Quantity]) ->
+    Mod = State#state.mod,
+
+    case gen_tcp:recv(Socket, 0, 3000) of
+        {ok, Data} -> 
+            <<1:16, 0:16, _:16, Dev_num:8, ?FUN_CODE_READ_IREGS:8, _:8, BinData/binary>> = Data,
+            LData = bin_to_list16(BinData, []),
+            Mod:message({input_register, Dev_num, Reg_num, LData}, State#state.state);
+
+        {error, Reason} ->
+            Mod:message({error, Reason}, State#state.state)
+
+    end.
+
+reading_coil_status(Socket, State, [Dev_num, Reg_num]) ->
+    Mod = State#state.mod,
+
+    case gen_tcp:recv(Socket, 0, 3000) of
+        {ok, Data} -> 
+            <<1:16, 0:16, 5:16, Dev_num:8, ?FUN_CODE_READ_COILS:8, Reg_value/binary>> = Data,
+            Mod:message({coils_status, Dev_num, Reg_num, Reg_value}, State#state.state);
+
+        {error, Reason} ->
+            Mod:message({error, Reason}, State#state.state)
 
     end.
 
@@ -206,17 +404,17 @@ writing_holding_register(Socket, State, [Dev_num, Reg_num, Values]) ->
     Mod = State#state.mod,
     Quantity = length(Values),
 
-    case gen_tcp:recv(Socket, 12, 3000) of
+    case gen_tcp:recv(Socket, 0, 3000) of
         {ok, Data} -> 
             <<1:16, 0:16, 6:16, Dev_num:8, ?FUN_CODE_WRITE_HREGS:8, _:16, Quantity:16>> = Data,
-            Mod:message([Dev_num, Reg_num, Values, Quantity], State#state.state);
+            Mod:message({holding_register, Dev_num, Reg_num, Values, Quantity}, State#state.state);
 
         {error, Reason} ->
-            Mod:message([error, Reason], State#state.state)
+            Mod:message({error, Reason}, State#state.state)
 
     end.
 
-try_reconnect(State, [Ip_addr, Port]) ->
+try_reconnect(State, [Ip_addr, Port], Iter) when Iter =< 2 ->
     % Поодключение к Modbus TCP устройству
     case {_, Socket} = gen_tcp:connect(Ip_addr, Port, ?SOCK_OPTS) of
         {ok, _} ->
@@ -224,7 +422,19 @@ try_reconnect(State, [Ip_addr, Port]) ->
                                                 ip_addr = Ip_addr, port = Port}};
 
         {error, _Reason} ->
-            try_reconnect(State, [Ip_addr, Port])
+            try_reconnect(State, [Ip_addr, Port], Iter + 1)
+
+    end;
+
+try_reconnect(State, [Ip_addr, Port], _Iter) ->
+    % Поодключение к Modbus TCP устройству
+    case {_, Socket} = gen_tcp:connect(Ip_addr, Port, ?SOCK_OPTS) of
+        {ok, _} ->
+            State#state{modbus_state = #modbusTCP{socket = Socket, connection = connect,
+                                                ip_addr = Ip_addr, port = Port}};
+
+        {error, Reason} ->
+            {error, Reason}
 
     end.
 
@@ -234,3 +444,8 @@ list_to_bin16([], Acc) ->
 list_to_bin16([H | T], Acc) ->
     list_to_bin16(T, <<Acc/binary, H:16>>).
 
+bin_to_list16(<<>>, Acc) ->
+    lists:reverse(Acc);
+
+bin_to_list16(<<H:16, T/binary>>, Acc) ->
+    bin_to_list16(T, [H | Acc]).
--- src/test.erl
+++ src/test.erl
@@ -5,52 +5,93 @@
 -export([
     start/0,
     stop/0,
-    init_modbus/1,
-    connect/1,
-    disconnect/1,
     connect_to/1,
     disconnect_from/0,
-    message/2,
     read_hreg/2,
-    write_hreg/3]).
+    read_iregs/3,
+    read_hregs/3,
+    write_hreg/3,
+    read_creg/2]).
+
+% modbusTCP callbacks
+-export([
+    init_modbus/1,
+    connect/2,
+    disconnect/1,
+    message/2,
+    terminate_modbus/2]).
+
+-define(SERVER, mtcp).
 
 start() ->
-    modbusTCP:start_link({local, tcp}, ?MODULE, [], []).
+    modbusTCP:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 stop() ->
-    modbusTCP:stop(tcp).
+    modbusTCP:stop(?SERVER).
 
 connect_to([Ip_addr, Port]) ->
-    modbusTCP:try_connect(tcp, [Ip_addr, Port]).
+    modbusTCP:try_connect(?SERVER, [Ip_addr, Port]).
 
 disconnect_from() ->
-    modbusTCP:try_disconnect(tcp).
+    modbusTCP:try_disconnect(?SERVER).
 
 read_hreg(Dev_num, Reg_num) ->
-    modbusTCP:read_register(tcp, {holding_register, Dev_num, Reg_num}).
+    modbusTCP:read_register(?SERVER, {holding_register, Dev_num, Reg_num}).
 
 write_hreg(Dev_num, Reg_num, Values) ->
-    modbusTCP:write_register(tcp, {holding_register, Dev_num, Reg_num, Values}).
+    modbusTCP:write_register(?SERVER, {holding_register, Dev_num, Reg_num, Values}).
+
+read_hregs(Dev_num, Reg_num, Quantity) ->
+    modbusTCP:read_register(?SERVER, {holding_register, Dev_num, Reg_num, Quantity}).
+
+read_iregs(Dev_num, Reg_num, Quantity) ->
+    modbusTCP:read_register(?SERVER, {input_register, Dev_num, Reg_num, Quantity}).
+
+read_creg(Dev_num, Reg_num) ->
+    modbusTCP:read_register(?SERVER, {coil_status, Dev_num, Reg_num}).
 
 init_modbus([]) ->
     {ok, 5, ["localhost", 502]}.
 
-connect(State) ->
-    {ok, State, ["localhost", 502]}.
+connect(State, Info) ->
+    case Info of
+        {error, Reason} ->
+            io:format("Error: ~w~n", [Reason]);
+        {connected, Ip_addr, Port} ->
+            io:format("connected to ~w ~w~n", [Ip_addr, Port]);
+        Info ->
+            io:format("connected fine~n")
+    end,
+    {ok, State, 0}.
 
 disconnect(State) ->
+    io:format("Disconect was fine.~n"),
     {ok, State}.
 
 message(RegisterInfo, State) ->
     case RegisterInfo of
-        [Dev_num, Reg_num, Reg_value] ->
+        {holding_register, Dev_num, Reg_num, LData} when is_list(LData) ->
+            io:format("Device number is ~w, first register number is ~w, register values is ~w.~n", [Dev_num, Reg_num, LData]);
+
+        {holding_register, Dev_num, Reg_num, Reg_value} ->
             io:format("Device number is ~w, register number is ~w, register value is ~w.~n", [Dev_num, Reg_num, Reg_value]);
 
-        [Dev_num, Reg_num, Values, _Quantity] ->
+        {holding_register, Dev_num, Reg_num, Values, _Quantity} ->
             io:format("Device number is ~w, register number is ~w, register values is ~w.~n", [Dev_num, Reg_num, Values]);
+
+        {input_register, Dev_num, Reg_num, LData} when is_list(LData) ->
+            io:format("Device number is ~w, first register number is ~w, register values is ~w.~n", [Dev_num, Reg_num, LData]);
         
-        [error, Reason] ->
+        {coil_status, Dev_num, Reg_num, Data} ->
+            io:format("Device number is ~w, first register number is ~w, register values is ~w.~n", [Dev_num, Reg_num, Data]);
+
+        
+        {error, Reason} ->
             io:format("Error: ~w~n", [Reason])
 
     end,
     {noreply, State}.
+
+terminate_modbus(_Reason, _State) ->
+    io:format("terminating~n"),
+    ok.
